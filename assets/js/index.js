72095411e7a2a075214b215ea7da4ebb5d57b562
khaisendefo <khaisendefo@gmail.com>
2024-06-18 00:04:00 +0300

    new section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+Fancybox.bind()
+
 document.addEventListener('DOMContentLoaded', function () {
   var splide = new Splide('.service__splide ', {
       perPage: 1,
@@ -10,6 +12,23 @@ document.addEventListener('DOMContentLoaded', function () {
       splide.go('<');
   });
   document.querySelector('.services__arrow-next').addEventListener('click', function() {
+      splide.go('>');
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var splide = new Splide('.gallery__slider', {
+      perPage: 1,
+      autoplay: false,
+      gap: '20px',
+      pagination: false,
+      arrows: false, 
+  }).mount();
+  document.querySelector('.gallery__arrow-prev').addEventListener('click', function() {
+      splide.go('<');
+  });
+
+  document.querySelector('.gallery__arrow-next').addEventListener('click', function() {
       splide.go('>');
   });
 });
