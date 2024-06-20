8fd0da9fc2c3b9e2ba4aa1fc422bc8c9e904c289
khaisendefo <khaisendefo@gmail.com>
2024-06-20 14:33:08 +0300

    new com

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,36 @@
 Fancybox.bind()
 
+function toggle() {
+    document.addEventListener('DOMContentLoaded', function () {
+      const toggleButton = document.querySelector('.header__toggle');
+      const sidebar = document.querySelector('.header__nav');
+      const navLinks = document.querySelectorAll('.header__nav-link');
+      function closeSidebar() {
+        toggleButton.classList.remove('active');
+        sidebar.classList.remove('active');
+      }
+      toggleButton.addEventListener('click', function () {
+        toggleButton.classList.toggle('active');
+        sidebar.classList.toggle('active');
+      });
+      document.addEventListener('click', function(event) {
+        if (!sidebar.contains(event.target) && !toggleButton.contains(event.target)) {
+          closeSidebar();
+        }
+      });
+      document.addEventListener('keydown', function(event) {
+        if (event.key === 'Escape' && sidebar.classList.contains('active')) {
+          closeSidebar();
+        }
+      });
+      navLinks.forEach(function(navLink) {
+        navLink.addEventListener('click', function() {
+          closeSidebar();
+        });
+      });
+    });
+} toggle();
+
 document.addEventListener('DOMContentLoaded', function () {
   var splide = new Splide('.service__splide ', {
       perPage: 1,
@@ -32,3 +63,5 @@ document.addEventListener('DOMContentLoaded', function () {
       splide.go('>');
   });
 });
+
+
